7a8033cca600fa2951581278bed3dd776896a5b9
Goodwu <goodwu@gmail.com>
2026-09-20 16:44:39 +0800

    fix(ohos): export vulkan dependency for mpv

--- scripts/libplacebo.sh
+++ scripts/libplacebo.sh
@@ -32,4 +32,34 @@ meson setup .. \
 ninja -j$CORES
 ninja install
 
+# libplacebo resolves Vulkan through its recursive Vulkan-Headers submodule, but
+# mpv discovers Vulkan through pkg-config in a separate Meson project. Export
+# the same headers and OHOS Vulkan loader as a normal Vulkan package so mpv can
+# resolve the dependency without relying on host Vulkan development files.
+VULKAN_HEADERS_DIR=$ROOT_DIR/libmpv/libplacebo/3rdparty/Vulkan-Headers/include
+if [ ! -d "$VULKAN_HEADERS_DIR/vulkan" ]; then
+	printf 'Vulkan headers are missing: %s\n' "$VULKAN_HEADERS_DIR" >&2
+	exit 1
+fi
+
+mkdir -p "$DEST/include" "$DEST/lib/pkgconfig"
+cp -R "$VULKAN_HEADERS_DIR/vulkan" "$DEST/include/"
+VULKAN_HEADER_VERSION=$(sed -n 's/^#define VK_HEADER_VERSION \([0-9][0-9]*\)$/\1/p' "$VULKAN_HEADERS_DIR/vulkan/vulkan_core.h" | head -n 1)
+VULKAN_VERSION=1.4.$VULKAN_HEADER_VERSION
+if [ -z "$VULKAN_HEADER_VERSION" ]; then
+	printf 'Unable to determine Vulkan header version\n' >&2
+	exit 1
+fi
+cat > "$DEST/lib/pkgconfig/vulkan.pc" <<EOF
+prefix=$DEST
+includedir=\${prefix}/include
+libdir=$OHOS_NDK_HOME/native/sysroot/usr/lib
+
+Name: Vulkan
+Description: Vulkan loader for OpenHarmony
+Version: $VULKAN_VERSION
+Libs: -L\${libdir} -lvulkan
+Cflags: -I\${includedir}
+EOF
+
 popd
